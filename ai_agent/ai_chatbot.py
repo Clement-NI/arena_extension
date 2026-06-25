de70477ec88644ec6df637948b9186746831523e
Clement-NI <niyuzhen2020@gmail.com>
2026-06-25 18:19:31 +0200

    archtecture of ai agent

--- ai_agent/ai_chatbot.py
+++ ai_agent/ai_chatbot.py
@@ -1,2 +1,5 @@
-import OS
+##
+## Here is the enterance of my ai agent. We shall create(initiate) an agent here with any LLM that can be declared
+## And we start this to launch the dialogue
+
 print("Hello, World!")
---
+++ ai_agent/system_prompts/system_prompt.py
@@ -0,0 +1,7 @@
+'''
+This is my system prompt for my ai agent
+So here you can direct my agent to create the correct json or yaml file for the arena project
+For configuration of the cluster : nodes.json + kind-config-template.json
+For configuration of the chaosmesh of the cluster : node.json + typology.yaml
+We don't generate the configuration directly but use the tool to avoid the hallucination
+'''
