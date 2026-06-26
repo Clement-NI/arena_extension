28729dc86644df04da6d59c8c08e3ce46cb93a00
Clement-NI <niyuzhen2020@gmail.com>
2026-06-26 17:11:34 +0200

    Modification

---
+++ ai_agent/cli/cli.py
@@ -0,0 +1,4 @@
+'''
+This is cli for our ai agent.
+Here we start the ai agent and interact with him using stream in a loop
+'''
--- ai_agent/system_prompts/system_prompt.py
+++ ai_agent/system_prompts/system_prompt.py
@@ -4,4 +4,8 @@ So here you can direct my agent to create the correct json or yaml file for the 
 For configuration of the cluster : nodes.json + kind-config-template.json
 For configuration of the chaosmesh of the cluster : node.json + typology.yaml
 We don't generate the configuration directly but use the tool to avoid the hallucination
+The steps in detail :
+1. generate node.json, kind-cluster-template.json (with which the arena will generate the kind-cluster-template.json)
+2. generate typology.yaml and generate the chaos.yaml with node.json and typology.yaml
+3. verify that the files generated can be correct without error.
 '''
