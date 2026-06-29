8432a6c6c63658827833a7b76cfb3533796f40f5
Clement-NI <niyuzhen2020@gmail.com>
2026-06-29 12:07:29 +0200

    Update generation_tool.py

--- ai_agent/utils/agent_tools/generation_tool.py
+++ ai_agent/utils/agent_tools/generation_tool.py
@@ -1,5 +1,5 @@
 '''
-This is our generation tool. It servers as a generator of node.json, kind-cluster-template and the topology.yaml
+This is our generation tool. It servers as a generator of node.json, kind-cluster-template.json and the topology.yaml
 as we describe in the prompt
 It should detect what we need and what we lack and ask us how to do.
 '''
